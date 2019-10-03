970661797156817f9c680272403f5a5eacb1584d
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 20:19:46 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,7 +19,7 @@ def bonus
       ]
    }
   }
-
+describe "bonus" do
   it "uses the []= method to change the hero and heroine's status from alive to dead" do
     expect(bonus[:montague][:hero][:status]).to eq("dead")
     expect(bonus[:capulet][:heroine][:status]).to eq("dead")
