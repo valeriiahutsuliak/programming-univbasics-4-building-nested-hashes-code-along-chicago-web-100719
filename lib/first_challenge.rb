9c5546e9ea8ec4fcc1b8dae25dd920e0037124f0
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 20:11:40 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,4 +14,4 @@ end
   :capulet => {
     :patriarch => {name: "Lord Capulet", age: "50"},
     :matriarch => {name: "Lady Capulet", age: "51"},
-    
+    :heroine => {name: "Juliet", age: "15", status: "alive"},
